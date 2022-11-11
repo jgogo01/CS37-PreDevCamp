cafc2a8408e0a7f7760db871868b968a8b6b2b52
Natdanai Pinves <83464496+jgogo01@users.noreply.github.com>
2022-11-11 15:45:35 +0700

    V0.1
    Navbar V1

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
 
---
+++ src/views/home.vue
@@ -0,0 +1,15 @@
+<script>
+import navbar from '../components/navbar.vue'
+export default {
+  components:{
+    navbar,
+  }
+}
+</script>
+
+<template>
+  <navbar/>
+</template>
+
+<style>
+</style>
