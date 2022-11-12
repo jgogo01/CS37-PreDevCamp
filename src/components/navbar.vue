01db6058089025482a2aa2de820b60e485f9a223
Natdanai Pinves <83464496+jgogo01@users.noreply.github.com>
2022-11-12 14:59:50 +0700

    Navbar Add Image

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg fixed-top navbar-light">
   <div class="container">
     <a class="navbar-brand text" href="#">Pre-DevCamp #9</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,6 +21,8 @@
     </div>
   </div>
 </nav>
+
+<img src="../assets/image/DevCamp9.gif" class="img-fluid" alt="Baner Image">
 </template>
 
 <script>
