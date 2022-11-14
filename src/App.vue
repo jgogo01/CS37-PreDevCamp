25e01bdd170af68b898916c3dd7c39d37ccfb559
Natdanai Pinves <83464496+jgogo01@users.noreply.github.com>
2022-11-14 16:08:45 +0700

    Nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
-<template>
-  <router-view/>
-</template>
-
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <navbar/>
+  <router-view/>
+  <footer/>
+</template>
+
+<script>
+import navbar from "./components/structure/navbar.vue";
+import footer from "./components/structure/footer.vue";
+export default {
+  components: {
+    navbar,
+    footer
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -8,22 +8,7 @@ import './assets/css/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-/* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-library.add(faUserSecret)
-/* add icons to the library */
-import {fab} from '@fortawesome/free-brands-svg-icons'
-library.add(fab);
-import {far} from '@fortawesome/free-regular-svg-icons'
-library.add(far)
-
-
 const app = createApp(App)
-app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
